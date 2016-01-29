cbde1d4d0619d1e5b07cc164740efa2980815e69
Bryan English <bryan@bryanenglish.com>
2016-01-29 14:24:54 -0800

    use signal-exit

--- index.js
+++ index.js
@@ -9,32 +9,16 @@ See LICENSE.txt
 var profiler = require('v8-profiler');
 var path = require('path');
 var Module = require('module');
+var onExit = require('signal-exit');
+
+var prefix = process.env.CLI_PROFILE_NAME || 'profile';
 
 process.argv.splice(1, 1);
-var origExit = process.exit;
-var exited = false;
-
-function end(cb) {
-    return function () {
-        if (exited) return;
-        exited = true;
-        var profile = profiler.stopProfiling();
-        var profileFileName = (process.env.CLI_PROFILE_NAME || 'profile') +
-            + Date.now() + '.cpuprofile';
-        profileFileName = path.join(process.cwd(), profileFileName);
-        profile.export()
-        .pipe(require('fs').createWriteStream(profileFileName))
-        .on('finish', function(){
-            profile.delete();
-            if (typeof cb === 'function') {
-                cb(arguments);
-            }
-        });
-    };
-}
-process.exit = end(function(args){
-    origExit.apply(process, args);
+onExit(function end () {
+    var profile = profiler.stopProfiling();
+    var file = path.join(process.cwd(), prefix + Date.now() + '.cpuprofile');
+    require('fs').writeFileSync(file, JSON.stringify(profile));
+    profile.delete();
 });
-process.on('beforeExit', end());
 profiler.startProfiling();
 process.nextTick(Module.runMain);
